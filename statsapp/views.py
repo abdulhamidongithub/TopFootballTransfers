4094c8eea5e7bb3fd11c71b116442d2d38385573
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-02-17 15:50:18 +0300

    Views optimized

--- statsapp/views.py
+++ statsapp/views.py
@@ -9,28 +9,28 @@ class StatsView(View):
 class ExpenditureView(View):
     def get(self, request):
         data = {
-            "expenditures":Expenditure_Records.objects.all().order_by("expenditure")
+            "expenditures":Expenditure_Records.objects.all().order_by("expenditure")[:50]
         }
         return render(request, "top-50-clubs-by-expenditure-in-2021.html", data)
 
 class IncomeView(View):
     def get(self, request):
         data = {
-            "income": Income_Records.objects.all().order_by("income")
+            "income": Income_Records.objects.all().order_by("income")[:50]
         }
         return render(request, "top-50-clubs-by-income-in-2021.html", data)
 
 class RecordTransfersView(View):
     def get(self, request):
         data = {
-            "transfers":Transfer.objects.all().order_by("-value")[:151]
+            "transfers":Transfer.objects.filter(value__gt=49.9).order_by("-value")[:150]
         }
         return render(request, "transfer-records.html", data)
 
 class PredictionsView(View):
     def get(self, request):
         data = {
-            "predictions": Predictions.objects.all().order_by("divergence", "-value")[:151]
+            "predictions": Predictions.objects.all().order_by("divergence", "-value")[:150]
         }
         return render(request, "150-accurate-predictions.html", data)
 
